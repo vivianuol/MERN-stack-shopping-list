93e06850c6943df876ac936f14b4a1b1caa10c4e
peng <sipeng_vivian@hotmail.com>
2020-11-17 14:43:06 -0600

    changed axios baseURL to default

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -1,12 +1,12 @@
  import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING } from './types';
  import axios from 'axios';
 
- const instance = axios.create({baseURL: 'http://192.168.0.24:3300'})
+ //const instance = axios.create({baseURL: 'http://192.168.0.24:3300'})
 
  
  export const getItems = () => dispatch => {
      dispatch(setItemsLoading());
-     instance.get('/api/items').then( res => 
+     axios.get('/api/items').then( res => 
         dispatch({
             type: GET_ITEMS,
             payload: res.data
@@ -16,7 +16,7 @@
 
  export const addItem = item => dispatch => {
      //dispatch(setItemsLoading());
-     instance
+     axios
         .post('/api/items', item)
         .then(res => {
             console.log("got response...." + JSON.stringify(res.data))
@@ -31,7 +31,7 @@
 
  export const deleteItem = id => dispatch => { 
      
-      instance.delete(`/api/items/${id}`).then(res => 
+      axios.delete(`/api/items/${id}`).then(res => 
         dispatch({
             type: DELETE_ITEM,
             payload: id
